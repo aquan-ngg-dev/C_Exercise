5c6c231b3afa96b7f7ba7e38742cbc1f56623911
Nguyen Anh Quan <nanhquan143@gmail.com>
2023-05-02 23:14:17 +0700

    Add Lesson1.c file

---
+++ Lesson1.c
@@ -0,0 +1,19 @@
+// BUỔI 1:GIỚI THIỆU VỀ XÂU KÝ TỰ|MẢNG KÝ TỰ TRONG NGÔN NGỮ C
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(){
+    //scanf("%s", a); //Dừng nhập khi gặp dấu cách,dấu tab,dấu enter,dấu end line, dấu end of file (EOF),thừa phím enter ở bộ đệm
+    //gets(a); //Dừng nhập khi gặp dấu xuống dòng, dấu EOF, không thừa phím enter
+    //cả scanf và gets cho phép nhập quá số kí tự tối đa, các kí tự thừa được lưu vào bộ nhớ không hợp lệ làm chương trình bị sai
+    //scanf("%10s",a); //Nhập tối đa 10 ký tự
+   int x;
+   scanf("%d",&x);
+   //getchar(); //Đọc 1 ký tự từ bàn phím, có thể thay thể bằng scanf("\n");
+   scanf("\n");
+   char a[10005];
+   gets(a);
+   printf("%s",a);
+}
